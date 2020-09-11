d7152b5916371130ac39c9fa66b6bf7d6822ceb9
gaabicarp <gaabicarp@gmail.com>
2020-09-11 19:32:20 -0300

    Agregando create al controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,20 @@
 class LinksController < ApplicationController
     #GET links
     def index
-        @links = Links.all
+        @links = Link.all
+        render json: @links
     end    
+    def show
+        @links = Link.find(params[:id])
+        render json: @links
+    end
+    def new
+        @links = Link.new
+    end
+    def create
+        @link = Link.new(short_url: params[:links][:short_url],
+                         original_url: params[:links][:original_url])
+        @link.save
+        redirect_to @link
+    end
 end
