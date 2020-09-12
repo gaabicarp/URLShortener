5a100664d5cd56a8a917ab42665787f92bf64d10
gaabicarp <gaabicarp@gmail.com>
2020-09-12 01:02:10 -0300

    Creando Servicios para generar shorturl

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
     #uniqness
-    validates :original_url, presence: true, length: {minimum: 10}
+    #validates :original_url, presence: true, length: {minimum: 10}
+    validates_presence_of :original_url
+    validates_uniqueness_of: short_url
 end
